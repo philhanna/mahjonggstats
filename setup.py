5bf39a7ac8f2a8db42ac40e77c8a54dc1f0b50a6
saspeh <ph1204@gmail.com>
2018-08-28 22:50:57 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mahjonggstats',
+    version='',
+    packages=['mj'],
+    url='',
+    license='',
+    author='Phil Hanna',
+    author_email='ph1204@gmail.com',
+    description=''
+)
