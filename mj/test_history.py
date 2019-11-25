b0fd11f83c4c6130bab328ea3c5280060a839464
Phil Hanna <ph1204@gmail.com>
2019-11-25 12:37:10 -0500

    More unit tests for format_time

--- mj/test_history.py
+++ mj/test_history.py
@@ -5,8 +5,32 @@ from mj.history import History
 
 class TestHistory(TestCase):
 
-    def test_format_time(self):
+    def test_hour_plus(self):
         seconds = 3603
         expected = "01:00:03"
+        actual = History.format_time(seconds)
+        self.assertEqual(expected, actual)
+
+    def test_empty(self):
+        seconds = 0
+        expected = "00:00"
+        actual = History.format_time(seconds)
+        self.assertEqual(expected, actual)
+
+    def test_one_minute(self):
+        seconds = 60
+        expected = "01:00"
+        actual = History.format_time(seconds)
+        self.assertEqual(expected, actual)
+
+    def test_two_seconds(self):
+        seconds = 2
+        expected = "00:02"
+        actual = History.format_time(seconds)
+        self.assertEqual(expected, actual)
+
+    def test_all_day(self):
+        seconds = 60*60*24 - 1
+        expected = "23:59:59"
         actual = History.format_time(seconds)
         self.assertEqual(expected, actual)
