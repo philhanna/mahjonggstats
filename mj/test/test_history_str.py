679983f458ec3e564a8df9c404c851d56c039b3f
Phil Hanna <ph1204@gmail.com>
2022-08-05 11:38:11 -0400

    Updated unit tests and removed unused variable

--- mj/test/test_history_str.py
+++ mj/test/test_history_str.py
@@ -7,7 +7,6 @@ class TestHistoryStr(TestCase):
     def test_str_hour_plus(self):
         line = "2019-11-25T12:21:58-0500 cloud 3603"
         history = History(line)
-        h_string = str(history)
         expected = "11/25/2019: 01:00:03"
         actual = str(history)
         self.assertEqual(expected, actual)
@@ -15,7 +14,6 @@ class TestHistoryStr(TestCase):
     def test_str_9_30(self):
         line = "2019-11-25T12:21:58-0500 cloud 570"
         history = History(line)
-        h_string = str(history)
         expected = "11/25/2019: 09:30"
         actual = str(history)
         self.assertEqual(expected, actual)
