07b5832dbb2b0a13d550ee3e5bd0ca5c51ecdb50
Phil Hanna <ph1204@gmail.com>
2022-09-28 20:58:20 -0400

    Refactored unit tests

--- mj/history.py
+++ mj/history.py
@@ -9,6 +9,7 @@ class History:
 
     @staticmethod
     def format_time(seconds) -> str:
+        """Creates a string with hh:mm:ss from the specified number of seconds"""
         seconds = int(seconds)
         mm = seconds // 60
         ss = seconds % 60
@@ -20,12 +21,15 @@ class History:
         return time_string
 
     def __init__(self, line):
+        """Creates a new History record from a line from the history file.
+        The line has the format (datetime level seconds)."""
         tokens = line.split()
         self.date: datetime = datetime.strptime(tokens[0], '%Y-%m-%dT%H:%M:%S%z')
         self.level: str = tokens[1]
         self.seconds: int = int(tokens[2])
 
     def __str__(self):
+        """Converts this object into a string representation"""
         tokens = (
             self.date.strftime("%m/%d/%Y"),
             self.level,
@@ -38,4 +42,5 @@ class History:
         return f"{self.seconds_as_mmss()} ({self.date.strftime('%m/%d/%Y')})"
 
     def seconds_as_mmss(self) -> str:
+        """Converts a number of seconds into hh:mm:ss"""
         return History.format_time(self.seconds)
