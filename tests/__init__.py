cde98e94af14b3121c4213dfc1a6a1d3b1e85031
Phil Hanna <ph1204@gmail.com>
2022-09-28 21:42:29 -0400

    Used better definition of testdata directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 import os.path
 
-project_root_dir = os.path.abspath("..")
+test_init = os.path.abspath(__file__)
+tests = os.path.dirname(test_init)
+project_root_dir = os.path.dirname(tests)
 testdata = os.path.join(project_root_dir, "testdata")
 
 __all__ = ['project_root_dir', 'testdata']
