e913cf6d55f18f9611e6e7e336207b6e1918c196
Phil Hanna <ph1204@gmail.com>
2022-10-02 14:26:40 -0400

    Bumped version number to 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mahjonggstats',
-    version='2.0.0',
+    version='2.0.1',
     packages=['mj'],
     url='https://github.com/philhanna/mahjonggstats',
     license='MIT license',
