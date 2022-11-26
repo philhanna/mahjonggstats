aff641220d45770dc9470259942a0206f89e74fc
Phil Hanna <ph1204@gmail.com>
2022-11-25 22:51:37 -0500

    Converted HistoryLine tests to PyTest

--- tests/test_history_line.py
+++ tests/test_history_line.py
@@ -1,64 +1,52 @@
-from unittest import TestCase
+from datetime import datetime, date, time
 
 from mj import HistoryLine
 
 
-class TestHistoryLine(TestCase):
-
-    def test_str_hour_plus(self):
-        line = "2019-11-25T12:21:58-0500 cloud 3603"
-        history = HistoryLine(line)
-        expected = "11/25/2019 cloud 01:00:03"
-        actual = str(history)
-        self.assertEqual(expected, actual)
-
-    def test_str_9_30(self):
-        line = "2019-11-25T12:21:58-0500 cloud 570"
-        history = HistoryLine(line)
-        expected = "11/25/2019 cloud 09:30"
-        actual = str(history)
-        self.assertEqual(expected, actual)
-
-    def test_timedate_normal(self):
-        line = "2019-11-30T20:05:00-0500 Normal 234"
-        h = HistoryLine(line)
-        expected = "03:54 (11/30/2019)"
-        actual = h.timedate()
-        self.assertEqual(expected, actual)
-
-    def test_format_time_hour_plus(self):
-        seconds = 3603
-        expected = "01:00:03"
-        actual = HistoryLine.format_time(seconds)
-        self.assertEqual(expected, actual)
-
-    def test_format_time_empty(self):
-        seconds = 0
-        expected = "00:00"
-        actual = HistoryLine.format_time(seconds)
-        self.assertEqual(expected, actual)
-
-    def test_format_time_one_minute(self):
-        seconds = 60
-        expected = "01:00"
-        actual = HistoryLine.format_time(seconds)
-        self.assertEqual(expected, actual)
-
-    def test_format_time_two_seconds(self):
-        seconds = 2
-        expected = "00:02"
-        actual = HistoryLine.format_time(seconds)
-        self.assertEqual(expected, actual)
+class TestHistoryLine:
 
     def test_format_time_all_day(self):
         seconds = 60 * 60 * 24 - 1
-        expected = "23:59:59"
-        actual = HistoryLine.format_time(seconds)
-        self.assertEqual(expected, actual)
+        assert HistoryLine.format_time(seconds) == "23:59:59"
+
+    def test_format_time_empty(self):
+        seconds = 0
+        assert HistoryLine.format_time(seconds) == "00:00"
+
+    def test_format_time_hour_plus(self):
+        seconds = 3603
+        assert HistoryLine.format_time(seconds) == "01:00:03"
+
+    def test_format_time_one_minute(self):
+        seconds = 60
+        assert HistoryLine.format_time(seconds) == "01:00"
+
+    def test_format_time_two_seconds(self):
+        seconds = 2
+        assert HistoryLine.format_time(seconds) == "00:02"
+
+    def test_game_date(self):
+        line = "2019-11-30T20:05:00-0500 Normal 234"
+        h = HistoryLine(line)
+        assert h.game_date.date() == date(2019, 11, 30)
+        assert h.game_date.time() == time(20,5,0)
 
     def test_repr(self):
         line = "2019-11-25T12:21:58-0500 cloud 3603"
         history = HistoryLine(line)
-        expected = f'HistoryLine("{line}")'
-        actual = repr(history)
-        self.assertEqual(expected, actual)
+        assert repr(history) == f'HistoryLine("{line}")'
+
+    def test_str_9_30(self):
+        line = "2019-11-25T12:21:58-0500 cloud 570"
+        history = HistoryLine(line)
+        assert str(history) == "2019-11-25 cloud 09:30"
+
+    def test_str_hour_plus(self):
+        line = "2019-11-25T12:21:58-0500 cloud 3603"
+        history = HistoryLine(line)
+        assert str(history) == "2019-11-25 cloud 01:00:03"
+
+    def test_timedate_normal(self):
+        line = "2019-11-30T20:05:00-0500 Normal 234"
+        h = HistoryLine(line)
+        assert h.timedate() == "03:54 (2019-11-30)"
