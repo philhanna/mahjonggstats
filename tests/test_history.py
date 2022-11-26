01f79092bcb11bbcb2b5fc7bfd8883e25ae30f06
Phil Hanna <ph1204@gmail.com>
2022-11-26 16:48:51 -0500

    Added unit test for latest_date

--- tests/test_history.py
+++ tests/test_history.py
@@ -2,7 +2,7 @@ from datetime import datetime, date
 
 from pytest import MonkeyPatch
 
-from mj import History
+from mj import History, HistoryLine
 from tests import testdata
 
 
@@ -22,13 +22,36 @@ class TestHistory:
         actual_date = self.history.earliest_date
         assert actual_date == expected_date
 
-'''
-    def test_get_level_history(self):
-        assert False
+    def test_get_level_history_easy(self):
+        actual = self.history.get_level_history("easy").records
+        expected = [
+            hl for hl in [HistoryLine(x) for x in testdata.splitlines()]
+            if hl.level_name == 'easy'
+        ]
+        assert actual == expected
+
+    def test_get_level_history_difficult(self):
+        actual = self.history.get_level_history("difficult").records
+        expected = [
+            hl for hl in [HistoryLine(x) for x in testdata.splitlines()]
+            if hl.level_name == 'difficult'
+        ]
+        assert actual == expected
+
+    def test_get_level_history_bogus(self):
+        actual = self.history.get_level_history("bogus").records
+        expected = [
+            hl for hl in [HistoryLine(x) for x in testdata.splitlines()]
+            if hl.level_name == 'bogus'
+        ]
+        assert actual == expected
 
     def test_latest_date(self):
-        assert False
+        expected_date = datetime.strptime("2022-08-06T23:07:24-0400", '%Y-%m-%dT%H:%M:%S%z')
+        actual_date = self.history.latest_date
+        assert actual_date == expected_date
 
+'''
     def test_level_names(self):
         assert False
 
