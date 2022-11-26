257f01602c214464407abe763c306064c890ac8d
Phil Hanna <ph1204@gmail.com>
2022-11-26 17:14:30 -0500

    Converted unit tests for History to pytest

--- tests/test_history.py
+++ tests/test_history.py
@@ -2,7 +2,7 @@ from datetime import datetime, date
 
 from pytest import MonkeyPatch
 
-from mj import History, HistoryLine
+from mj import History, HistoryLine, LevelHistory
 from tests import testdata
 
 
@@ -22,45 +22,58 @@ class TestHistory:
         actual_date = self.history.earliest_date
         assert actual_date == expected_date
 
-    def test_get_level_history_easy(self):
-        actual = self.history.get_level_history("easy").records
+    def help_test_level_names(self, level_name):
+        actual = self.history.get_level_history(level_name).records
         expected = [
             hl for hl in [HistoryLine(x) for x in testdata.splitlines()]
-            if hl.level_name == 'easy'
+            if hl.level_name == level_name
         ]
         assert actual == expected
+
+    def test_get_level_history_easy(self):
+        self.help_test_level_names("easy")
 
     def test_get_level_history_difficult(self):
-        actual = self.history.get_level_history("difficult").records
-        expected = [
-            hl for hl in [HistoryLine(x) for x in testdata.splitlines()]
-            if hl.level_name == 'difficult'
-        ]
-        assert actual == expected
+        self.help_test_level_names("difficult")
 
     def test_get_level_history_bogus(self):
-        actual = self.history.get_level_history("bogus").records
-        expected = [
-            hl for hl in [HistoryLine(x) for x in testdata.splitlines()]
-            if hl.level_name == 'bogus'
-        ]
-        assert actual == expected
+        self.help_test_level_names("bogus")
 
     def test_latest_date(self):
         expected_date = datetime.strptime("2022-08-06T23:07:24-0400", '%Y-%m-%dT%H:%M:%S%z')
         actual_date = self.history.latest_date
         assert actual_date == expected_date
 
-'''
     def test_level_names(self):
-        assert False
+        # Note: the list of level names is sorted by the mean solution time of each level
+        assert self.history.level_names == ['difficult', 'ziggurat', 'easy']
 
     def test_levels(self):
-        assert False
-
-    def test_load(self):
-        assert False
+        expected = {
+            'difficult': LevelHistory("difficult", [
+                HistoryLine("2022-08-05T23:50:36-0400 difficult 218")
+            ]),
+            'ziggurat': LevelHistory("ziggurat", [
+                HistoryLine("2022-08-06T22:57:13-0400 ziggurat 228")
+            ]),
+            'easy': LevelHistory("easy", [
+                HistoryLine("2022-07-31T01:51:05-0400 easy 308"),
+                HistoryLine("2022-08-04T22:27:39-0400 easy 243"),
+                HistoryLine("2022-08-06T23:02:17-0400 easy 171"),
+                HistoryLine("2022-08-06T23:07:24-0400 easy 294"),
+            ]),
+        }
+        actual = self.history.levels
+        assert actual == expected
 
     def test_records(self):
-        assert False
-'''
+        expected = [
+            HistoryLine("2022-07-31T01:51:05-0400 easy 308"),
+            HistoryLine("2022-08-04T22:27:39-0400 easy 243"),
+            HistoryLine("2022-08-05T23:50:36-0400 difficult 218"),
+            HistoryLine("2022-08-06T22:57:13-0400 ziggurat 228"),
+            HistoryLine("2022-08-06T23:02:17-0400 easy 171"),
+            HistoryLine("2022-08-06T23:07:24-0400 easy 294"),
+        ]
+        actual = self.history.records
+        assert actual == expected
