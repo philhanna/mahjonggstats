bd1ddd83246524da665eb59e67f0b7899ad3181a
Phil Hanna <ph1204@gmail.com>
2022-12-24 17:49:45 -0500

    Converted unit test class into individual pytest methods

--- tests/test_main_exceptions.py
+++ tests/test_main_exceptions.py
@@ -1,29 +1,21 @@
-from io import StringIO
-
 import pytest
-from pytest import MonkeyPatch
 
 from mj import Main, History
-from tests import testdata, stdout_redirected
+from tests import testdata
 
 
-class TestMainExceptions:
-
-    def test_get_history_with_no_history(self):
-        MonkeyPatch().setattr(History, 'load', lambda: [])
-        with StringIO() as out, stdout_redirected(out):
-            with pytest.raises(RuntimeError) as ae:
-                parms = {}
-                main = Main(**parms)
-            assert str(ae.value) == "No mahjongg history yet"
-
-    def test_bogus_level_name(self):
-        MonkeyPatch().setattr(History, 'load', lambda: [x for x in testdata.splitlines()])
-        with StringIO() as out, stdout_redirected(out):
-            with pytest.raises(ValueError) as ae:
-                parms = {'name': 'bogus' }
-                main = Main(**parms)
-                main.run()
-            assert str(ae.value) == "Level bogus not found"
+def test_get_history_with_no_history(monkeypatch):
+    monkeypatch.setattr(History, 'load', lambda: [])
+    with pytest.raises(RuntimeError) as ae:
+        parms = {}
+        Main(**parms)
+    assert str(ae.value) == "No mahjongg history yet"
 
 
+def test_bogus_level_name(monkeypatch):
+    monkeypatch.setattr(History, 'load', testdata.splitlines)
+    with pytest.raises(ValueError) as ae:
+        parms = {'name': 'bogus'}
+        main = Main(**parms)
+        main.run()
+    assert str(ae.value) == "Level bogus not found"
