a0d396eef3207ca6541b7bc99b22ad1bd6779a96
Phil Hanna <ph1204@gmail.com>
2022-12-24 17:47:09 -0500

    Converted unit test class into individual pytest methods

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,58 +1,58 @@
 from io import StringIO
 
-from pytest import MonkeyPatch
+import pytest
+
 from mj import Main, History
-from tests import stdout_redirected, testdata
+from tests import testdata
 
 
-class TestMain:
+@pytest.fixture
+def main(monkeypatch):
+    monkeypatch.setattr(History, "load", lambda: testdata.splitlines())
 
-    def setup_method(self):
-        MonkeyPatch().setattr(History, 'load', lambda: [x for x in testdata.splitlines()])
 
-    def test_init(self):
-        main = Main(**{})
-        assert main.level_name is None
-        assert not main.level_names_only
-        assert not main.verbose
+def test_init(main):
+    main = Main(**{})
+    assert main.level_name is None
+    assert not main.level_names_only
+    assert not main.verbose
 
-    def test_level_name(self):
-        parms = {'name': 'bogus'}
-        main = Main(**parms)
-        assert main.level_name == 'bogus'
 
-    def test_run(self):
-        with StringIO() as out, stdout_redirected(out):
-            parms = {'verbose': True}
-            main = Main(**parms)
-            main.run()
-            output = out.getvalue()
-        assert "σ" in output    # Unicode sigma = U+03C3
-        assert "μ" in output    # Unicode mu = U+03BC
+def test_level_name(main):
+    parms = {'name': 'bogus'}
+    main = Main(**parms)
+    assert main.level_name == 'bogus'
 
-    def test_run_level_names_only(self):
-        with StringIO() as out, stdout_redirected(out):
-            parms = {'level_names_only': True}
-            main = Main(**parms)
-            main.run()
-            output = out.getvalue()
-        assert "difficult\nziggurat\neasy\n" == output
 
-    def test_run_quiet(self):
-        with StringIO() as out, stdout_redirected(out):
-            parms = {}
-            main = Main(**parms)
-            main.run()
-            output = out.getvalue()
-        for outline in output.splitlines():
-            assert "average=" in outline
+def test_run(capsys, main):
+    parms = {'verbose': True}
+    main = Main(**parms)
+    main.run()
+    output = capsys.readouterr().out
+    assert "σ" in output  # Unicode sigma = U+03C3
+    assert "μ" in output  # Unicode mu = U+03BC
 
-    def test_run_level_name(self):
-        MonkeyPatch().setattr(History, 'load', lambda: [x for x in testdata.splitlines()])
-        with StringIO() as out, stdout_redirected(out):
-            parms = {'name': 'difficult' }
-            main = Main(**parms)
-            main.run()
-            output = out.getvalue()
-        assert "level \"difficult\"" in output
 
+def test_run_level_names_only(capsys, main):
+    parms = {'level_names_only': True}
+    main = Main(**parms)
+    main.run()
+    output = capsys.readouterr().out
+    assert "difficult\nziggurat\neasy\n" == output
+
+
+def test_run_quiet(main, capsys):
+    parms = {}
+    main = Main(**parms)
+    main.run()
+    output = capsys.readouterr().out
+    for outline in output.splitlines():
+        assert "average=" in outline
+
+
+def test_run_level_name(main, capsys):
+    parms = {'name': 'difficult'}
+    main = Main(**parms)
+    main.run()
+    output = capsys.readouterr().out
+    assert "level \"difficult\"" in output
