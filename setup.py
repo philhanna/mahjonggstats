9d19db8970baf33b3761b073cb75f736fea5b722
Phil Hanna <ph1204@gmail.com>
2022-12-24 17:54:03 -0500

    Bumped version to 2.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mahjonggstats',
-    version='2.2.0',
+    version='2.2.1',
     install_requires=['pytest'],
     packages=['mj'],
     url='https://github.com/philhanna/mahjonggstats',
