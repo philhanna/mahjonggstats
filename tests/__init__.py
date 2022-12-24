35e0d07a4a8cdccdd7d73a4651bde5f56e5f0b8f
Phil Hanna <ph1204@gmail.com>
2022-12-24 17:04:11 -0500

    Use pathlib instead of os.path.
    Remove stdout_redirected in favor of pytest.capsys

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,6 @@
-import os.path
-import sys
-from contextlib import contextmanager
+from pathlib import Path
 
-test_init = os.path.abspath(__file__)
-tests = os.path.dirname(test_init)
-project_root_dir = os.path.dirname(tests)
+project_root_dir = Path(__file__).parent.parent
 testdata = """\
 2022-07-31T01:51:05-0400 easy 308
 2022-08-04T22:27:39-0400 easy 243
@@ -13,21 +9,7 @@ testdata = """\
 2022-08-06T23:02:17-0400 easy 171
 2022-08-06T23:07:24-0400 easy 294"""
 
-
-# redirect stdout technique from https://www.python.org/dev/peps/pep-0343/
-
-@contextmanager
-def stdout_redirected(new_stdout):
-    save_stdout = sys.stdout
-    sys.stdout = new_stdout
-    try:
-        yield None
-    finally:
-        sys.stdout = save_stdout
-
-
 __all__ = [
-    'stdout_redirected',
     'project_root_dir',
     'testdata',
 ]
