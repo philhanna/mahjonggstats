5ba94773a6e7d62ac7322547d8881852de488eac
Phil Hanna <ph1204@gmail.com>
2023-02-05 06:57:44 -0500

    Added stub unit test

--- level_history_test.go
+++ level_history_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestLevelHistory_Confidence(t *testing.T) {
 	type fields struct {
@@ -27,6 +29,31 @@ func TestLevelHistory_Confidence(t *testing.T) {
 			}
 			if got1 != tt.want1 {
 				t.Errorf("LevelHistory.Confidence() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
+
+func TestLevelHistory_Mean(t *testing.T) {
+	type fields struct {
+		levelName string
+		records   []HistoryLine
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lh := LevelHistory{
+				levelName: tt.fields.levelName,
+				records:   tt.fields.records,
+			}
+			if got := lh.Mean(); got != tt.want {
+				t.Errorf("LevelHistory.Mean() = %v, want %v", got, tt.want)
 			}
 		})
 	}
