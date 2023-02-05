4d4cd807a5797127e8fc01db78e282746879e926
Phil Hanna <ph1204@gmail.com>
2023-02-05 07:36:28 -0500

    made almostEqual variadic
    Can be called with a third optional parameter, delta

--- testdata.go
+++ testdata.go
@@ -12,6 +12,12 @@ const testdata = `2022-07-31T01:51:05-0400 easy 308
 
 const almostThreshold = 1e-5
 
-func almostEqual(a, b float64) bool {
-	return math.Abs(a - b) <= almostThreshold
+func almostEqual(floats ...float64) bool {
+	a := floats[0]
+	b := floats[1]
+	delta := almostThreshold
+	if len(floats) > 2 {
+		delta = floats[2]
+	}
+	return math.Abs(a - b) <= delta
 }
