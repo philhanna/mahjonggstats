a0465015e87112d8d55dba64b385743ec2fef24c
Phil Hanna <ph1204@gmail.com>
2023-02-15 14:12:48 -0500

    Ported view to Go
    Fix for issue #8
    
    Squashed commit of the following:
    
    commit 1edd5410a1db9b1b5b94bf05a867edd3f3b6a117
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 14:08:16 2023 -0500
    
        finished ShowAllLevels
    
    commit 0b280f440e8b8eba8a5fe472e4273c373427df96
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 11:55:38 2023 -0500
    
        Internalized some functions
    
    commit cedd24a747a538e7cf076c405eb03e4a67d1d93a
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 11:53:15 2023 -0500
    
        Working on View
    
    commit 8b4d759069fb615255231a3580739614d820990b
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 10:47:50 2023 -0500
    
        Temporary commit - not working
    
    commit d10c86b8856860fccaebea4e7731ff27c001f8c4
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 10:00:37 2023 -0500
    
        Added constructor and ShowLevelNames
    
    commit ffddca8e59994737efb6cd11ee3e697f219e43a3
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 09:50:21 2023 -0500
    
        Defined View as an interface
    
    commit 0dc5c09b2935fc85ad0487b20342e4bf6f5ec205
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 00:50:29 2023 -0500
    
        Added stub of view.go
    
    commit 2540f99f43b18ec37e38341378f9c1b1464ec438
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 00:28:16 2023 -0500
    
        Updated README

--- README.md
+++ README.md
@@ -5,7 +5,24 @@ Go program to display statistics from Gnome mahjongg.
 This version is still under active development.
 The Python branch is fully functional.
 
+## Software architecture
+This project uses a [Model-View-Controller][idMVC] approach.
+Here are the types associated with each:
+
+### Model
+- `History`
+- `HistoryLine`
+- `LevelHistory`
+  
+### View
+- `View`
+
+### Controller
+- `cmd/mahjonggstats`
+
 ## References
 - [Github repository](https://github.com/philhanna/mahjonggstats)
 - [Github repository for gnome-mahjongg](https://github.com/GNOME/gnome-mahjongg)
 - [Gnome wiki for Mahjongg](https://wiki.gnome.org/Apps/Mahjongg)
+
+[idMVC]: https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller
---
+++ view.go
@@ -0,0 +1,133 @@
+package mj
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+// ---------------------------------------------------------------------
+// Type definitions
+// ---------------------------------------------------------------------
+
+// View presents the history on standard output.
+type View struct {
+	model      History
+	levelNames []string
+	levels     []LevelHistory
+}
+
+// ---------------------------------------------------------------------
+// Constructors
+// ---------------------------------------------------------------------
+
+// NewView creates a new View with the specified history model.
+func NewView(model History) View {
+	v := new(View)
+	v.model = model
+	v.levelNames = append(v.levelNames, model.LevelNames()...)
+	for _, level := range model.Levels {
+		v.levels = append(v.levels, level)
+	}
+	return *v
+}
+
+// ---------------------------------------------------------------------
+// Methods
+// ---------------------------------------------------------------------
+
+// ShowAllLevels displays the history for each level. 
+func (v View) ShowAllLevels() {
+	for levelName, levelHistory := range v.levels {
+
+		mean := levelHistory.Mean()
+		stndev := levelHistory.StandardDeviation()
+		lo, hi := levelHistory.Confidence()
+		count := levelHistory.Count()
+
+		meanString := FormatTime(int(mean))
+		stndevString := FormatTime(int(stndev))
+		loString := FormatTime(int(lo))
+		hiString := FormatTime(int(hi))
+		countString := pluralize(count, "game")
+
+		fmt.Printf("\n%d %s at level %q\n", count, countString, levelName)
+		fmt.Printf("\tμ\t= %s\n", meanString)
+		fmt.Printf("\tσ\t= %s\n", stndevString)
+		fmt.Printf("\trange\t= %s to %s (at 95%% confidence level)\n", loString, hiString)
+	
+		// Slice of five if there are fewer:
+		// https://go.dev/play/p/Kf85DFQ_8Lr
+		
+		// Create top5 with all the history, then sort it and select just the top 5.
+		// This is actually the five *shortest* times.
+		top5 := make([]HistoryLine, 0, len(levelHistory.Records)+5)
+		top5 = append(top5, levelHistory.Records...)
+		sort.Slice(top5, func(i, j int) bool {
+			return top5[i].Seconds < top5[j].Seconds
+		})
+		top5 = top5[:5]
+
+		scoreString := pluralize(len(top5), "score")
+		fmt.Printf("\ttop %s:\n", scoreString)
+		for _, h := range top5 {
+			fmt.Printf("\t\t  %s\n", h.TimeDate())	
+		}
+		fmt.Println()
+	}
+}
+
+// ShowHeading displays the number of games between the earliest and
+// latest date.
+func (v View) ShowHeading() {
+	count := len(v.model.Records)
+	start := v.model.EarliestDate().Format(time.DateOnly)
+	end := v.model.LatestDate().Format(time.DateOnly)
+	fmt.Printf("\nMahjongg history of %d games from %s to %s\n", count, start, end)
+}
+
+// ShowLevelNames displays the level names.
+func (v View) ShowLevelNames() {
+	for _, levelName := range v.levelNames {
+		fmt.Println(levelName)
+	}
+}
+
+// ShowSummary displays a summary of average times for each level.
+func (v View) ShowSummary() {
+
+	getPrefix := func(lh LevelHistory) string {
+		count := lh.Count()
+		gamesString := pluralize(count, "game")
+		levelName := lh.LevelName
+		return fmt.Sprintf("%d %s at level %q", count, gamesString, levelName)
+	}
+
+	prefixes := []string{}
+	for _, lh := range v.levels {
+		prefix := getPrefix(lh)
+		prefixes = append(prefixes, prefix)
+	}
+
+	maxPrefixLength := 0
+	for i, prefix := range prefixes {
+		length := len(prefix)
+		if i == 0 || length > maxPrefixLength {
+			maxPrefixLength = length
+		}
+	}
+
+	for _, lh := range v.levels {
+		prefix := getPrefix(lh)
+		part1 := fmt.Sprintf("%*s", maxPrefixLength, prefix)
+		part2 := fmt.Sprintf("average=%s", FormatTime(int(lh.Mean())))
+		fmt.Printf(part1 + " " + part2)
+	}
+}
+
+func pluralize(count int, name string) string {
+	if count == 1 {
+		return name
+	}
+	return name + "s"
+}
