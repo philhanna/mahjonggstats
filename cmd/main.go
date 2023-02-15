c604e3c28b89ed7793c3ebfef57333748f5d9e41
Phil Hanna <ph1204@gmail.com>
2023-02-15 16:45:11 -0500

    Fix for issue #10
    Squashed commit of the following:
    
    commit d9be9d92fd7c32d70829e47822701a6e857bbdd0
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 16:44:30 2023 -0500
    
        Added stub of controller
    
    commit c4d7eb7047b61594621a9ca24a2d5c65423884f3
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 14:51:22 2023 -0500
    
        Complete mainline
    
    commit b4dc9f6951d5e598090ba18237f733bf7672566c
    Author: Phil Hanna <ph1204@gmail.com>
    Date:   Wed Feb 15 14:50:50 2023 -0500
    
        Remove python filters from .gitignore

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import mj "github.com/philhanna/mahjonggstats"
+import "flag"
+
+func main() {
+	var name string
+	var levelNamesOnly bool
+	var verbose bool
+
+	flag.StringVar(&name, "n", "", "select only this level name")
+	flag.BoolVar(&levelNamesOnly, "l", false, "show level names only")
+	flag.BoolVar(&verbose, "v", false, "show detailed summary")
+	flag.Parse()
+
+	args := make(map[string]any)
+	args["n"] = name
+	args["l"] = levelNamesOnly
+	args["v"] = verbose
+	
+	model := mj.NewHistory()
+	view := mj.NewView(model)
+	controller := mj.NewController(model, view, args)
+	controller.Run()
+
+}
